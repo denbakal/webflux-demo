d7bdbcf2dd51a4734ceae6980cd5614f4f29b728
Bakal <d.bakal@controlpay.com>
2018-05-22 15:02:28 +0300

    Java 9: Spring Boot 2.0 + Lombok

--- src/main/java/ua/demo/controller/UserController.java
+++ src/main/java/ua/demo/controller/UserController.java
@@ -4,10 +4,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ua.demo.dto.UserDto;
 
+import java.util.Collections;
+import java.util.List;
+
 @RestController
 public class UserController {
     @GetMapping(value = "users")
-    public void getUsers() {
-        UserDto userDto = new UserDto();
+    public List<UserDto> getUsers() {
+        return Collections.emptyList();
     }
 }
--- src/main/java/ua/demo/dto/UserDto.java
+++ src/main/java/ua/demo/dto/UserDto.java
@@ -2,10 +2,16 @@ package ua.demo.dto;
 
 import lombok.*;
 
+import java.time.LocalDate;
+
 @Getter
 @Setter
 @EqualsAndHashCode
 @ToString
 @NoArgsConstructor
 public class UserDto {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private LocalDate birthday;
 }
